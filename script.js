870e350112e77d6834bd2927372fa426568a20da
Jonibek <munirovjonibek@gmail.com>
2022-04-03 09:14:23 +0500

    added recently

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ let btn = document.querySelector(".btn");
 let data = 0;
 let bigData = 0;
 let date = document.querySelector(".date");
+let recently1 = document.querySelector(".exchange-rate1");
+let recently2 = document.querySelector(".exchange-rate2");
+let recently3 = document.querySelector(".exchange-rate3");
 fetch("https://api.fastforex.io/fetch-all?api_key=cd819107bf-af7afd5e1e-r9qw6m")
   .then((response) => response.json())
   .then(function (el1) {
     bigData = el1;
     console.log(bigData);
     let cur = el1.results;
+    console.log(cur.USD);
+    console.log(cur.UZS);
+    recently1.textContent =
+      "1 USD = " + (cur.UZS / cur.USD).toFixed(3) + " SUM";
+    recently2.textContent =
+      "1 RUB = " + (cur.UZS / cur.RUB).toFixed(3) + " SUM";
     data = cur;
+    recently3.textContent =
+      "1 EUR = " + (cur.UZS / cur.EUR).toFixed(3) + " SUM";
+    data = cur;
+    date.textContent = `Last updated on ` + bigData.updated;
     for (let keys in cur) {
       select2(keys);
     }
@@ -35,5 +48,4 @@ function natija(e) {
   let select2 = document.getElementsByClassName("select2")[0].value;
   let res = (data[select2] / data[select1]) * a;
   result.textContent = `${a} ${select1} = ${res} ${select2}`;
-  date.textContent = `Last updated on ` + bigData.updated;
 }
