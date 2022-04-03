c3bf0959a0ce774304c5fd54c57102975364f0ff
Jonibek <munirovjonibek@gmail.com>
2022-04-03 09:19:53 +0500

    bug fix

--- script.js
+++ script.js
@@ -11,10 +11,8 @@ fetch("https://api.fastforex.io/fetch-all?api_key=cd819107bf-af7afd5e1e-r9qw6m")
   .then((response) => response.json())
   .then(function (el1) {
     bigData = el1;
-    console.log(bigData);
     let cur = el1.results;
-    console.log(cur.USD);
-    console.log(cur.UZS);
+
     recently1.textContent =
       "1 USD = " + (cur.UZS / cur.USD).toFixed(3) + " SUM";
     recently2.textContent =
@@ -44,6 +42,10 @@ window.addEventListener("keydown", function (e) {
 function natija(e) {
   e.preventDefault(e);
   let a = document.querySelector(".input").value;
+  if (a < 0) {
+    a *= -1;
+    document.querySelector(".input").value = a;
+  }
   let select1 = document.getElementsByClassName("select1")[0].value;
   let select2 = document.getElementsByClassName("select2")[0].value;
   let res = (data[select2] / data[select1]) * a;
